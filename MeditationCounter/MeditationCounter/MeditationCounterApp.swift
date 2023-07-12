a0f9a8ce0959910f62a31f061bdf42c8dc6ac537
danuta.zabinska <danuta.zabinska@jamf.com>
2023-07-12 16:11:08 +0200

    first project added-test-commit

---
+++ MeditationCounter/MeditationCounter/MeditationCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationCounterApp.swift
+//  MeditationCounter
+//
+//  Created by Danuta Żabińska on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditationCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
