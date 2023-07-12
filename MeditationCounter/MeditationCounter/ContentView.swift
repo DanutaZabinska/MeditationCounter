4ab056c47548b71beda2c97ce6df01357a8c98fd
DanutaZabinska <danuta.irena.zabinska@gmail.com>
2023-07-12 17:03:47 +0200

    test

--- MeditationCounter/MeditationCounter/ContentView.swift
+++ MeditationCounter/MeditationCounter/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, worldnnnn!")
+            Text("Hello, worldnnnn444!")
         }
         .padding()
     }
